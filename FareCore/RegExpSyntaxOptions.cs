a3bf4314968c1430e5c136a7d0c5c1a85f590abe
scherenhaenden <scherenhaenden@gmail.com>
2024-04-26 10:39:55 +0200

    Refactor namespaces to FareCore
    Move RegExpMatchingOptions, RegExpSyntaxOptions, and SpecialOperations to FareCore namespace. Improve overall code structure and consistency.

--- FareCore/RegExpSyntaxOptions.cs
+++ FareCore/RegExpSyntaxOptions.cs
@@ -1,40 +1,41 @@
-namespace RandomDataGeneratorCore.FareRegex;
-
-[Flags]
-public enum RegExpSyntaxOptions
+namespace FareCore
 {
-    /// <summary>
-    /// Enables intersection.
-    /// </summary>
-    Intersection = 0x0001,
+    [Flags]
+    public enum RegExpSyntaxOptions
+    {
+        /// <summary>
+        /// Enables intersection.
+        /// </summary>
+        Intersection = 0x0001,
 
-    /// <summary>
-    /// Enables complement.
-    /// </summary>
-    Complement = 0x0002,
+        /// <summary>
+        /// Enables complement.
+        /// </summary>
+        Complement = 0x0002,
 
-    /// <summary>
-    /// Enables empty language.
-    /// </summary>
-    Empty = 0x0004,
+        /// <summary>
+        /// Enables empty language.
+        /// </summary>
+        Empty = 0x0004,
 
-    /// <summary>
-    /// Enables anystring.
-    /// </summary>
-    Anystring = 0x0008,
+        /// <summary>
+        /// Enables anystring.
+        /// </summary>
+        Anystring = 0x0008,
 
-    /// <summary>
-    /// Enables named automata.
-    /// </summary>
-    Automaton = 0x0010,
+        /// <summary>
+        /// Enables named automata.
+        /// </summary>
+        Automaton = 0x0010,
 
-    /// <summary>
-    /// Enables numerical intervals.
-    /// </summary>
-    Interval = 0x0020,
+        /// <summary>
+        /// Enables numerical intervals.
+        /// </summary>
+        Interval = 0x0020,
 
-    /// <summary>
-    /// Enables all optional regexp syntax.
-    /// </summary>
-    All = 0xffff
+        /// <summary>
+        /// Enables all optional regexp syntax.
+        /// </summary>
+        All = 0xffff
+    }
 }
